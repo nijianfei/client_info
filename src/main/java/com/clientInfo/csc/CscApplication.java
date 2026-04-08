13c55e1292095ea4714c1cd0a6021ee17a8bc7a3
nijianfei <nijianfei@163.com>
2026-04-08 11:12:52 +0800

    优化门禁web检测重启机制

--- src/main/java/com/clientInfo/csc/CscApplication.java
+++ src/main/java/com/clientInfo/csc/CscApplication.java
@@ -14,22 +14,6 @@ public class CscApplication {
     public static void main(String[] args) {
         SpringApplication.run(CscApplication.class, args);
         log.info("应用已成功启动");
-//        ArpUtil.initClientInfoMap("cmd /c arp -a | findstr 10.65.208 |findstr  /V \\u63A5\\u53E3");
-//        //临时测试用
-//        List<String> ipList = FileUtil.readLines(new File(new File(System.getProperty("user.dir")), "ips.txt"), "utf8");
-//        ipList.forEach(ip-> SurveillanceItem.pingSync(ip));
-//        Runtime.getRuntime().addShutdownHook(new Thread(()->{
-//            Set<String> keySet = SurveillanceItem.processMap.keySet();
-//            for (String key : keySet) {
-//                Process process = SurveillanceItem.processMap.get(key);
-//                if (process != null) {
-//                    process.destroy();
-//                    System.out.println("ShutdownHook---------------> "+key+" isAlive:" + process.isAlive());
-//                }
-//            }
-//            System.out.println("ShutdownHook--------------->End");
-//        }));
-//        NlbUtil.queryInterfaceName();
     }
 
 }
